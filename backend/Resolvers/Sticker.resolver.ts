cb2e1408fdbf61a0c3a35fd66dd4f4cac4835e0d
carlos-benner <carlos.benner.b@gmail.com>
2024-07-08 19:10:31 +0200

    Creating sticker query. Adding eslint config

--- backend/Resolvers/Sticker.resolver.ts
+++ backend/Resolvers/Sticker.resolver.ts
@@ -1,9 +1,22 @@
 import { Stickers } from '../DummyData/index.js'
 
 const StickerResolver = {
-    Query: {
-        stickers: () => Stickers
+  Query: {
+    stickers: async (_, __, context) => Stickers,
+    sticker: async (_, { stickerId }) => {
+			try {
+        console.log(stickerId);
+        const sticker = Stickers.filter((sticker) => sticker._id === stickerId)
+        if (sticker.length > 0) {
+          return sticker[0];
+        }
+        return null;
+			} catch (err) {
+				console.error("Error getting sticker:", err);
+				throw new Error("Error getting transaction");
+			}
     }
+  }
 }
 
 export default StickerResolver;
--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,10 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["node_modules/", "frontend/.next/"]
+  },
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
